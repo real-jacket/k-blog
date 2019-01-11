6122e0d2bf4875e6bce9a8aeb3ed1ad0657e3340
xiaomu-deepin <1762982273@qq.com>
2019-01-12 00:35:52 +0800

    fix

--- middlewares/check.js
+++ middlewares/check.js
@@ -1,6 +1,8 @@
 module.exports = {
   checkLogin: function checkLogin(req,res,next){
     if(!req.session.user){
+      console.log('指向到login里面');
+
       req.flash('error','未登录')
       return res.redirect('/signin')
     }
@@ -9,6 +11,8 @@ module.exports = {
 
   checkNotLogin:function checkNotLogin(req,res,next){
     if(req.seesion.user){
+      console.log('执行到not里面');
+
       req.flash('error','已登录')
       return res.redirect('back') // 返回之前的页面
     }
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ module.exports = function(app){
   app.get('/',function(req,res){
     res.redirect('/posts')
   })
-  app.use('/singup',require('./singup'))
-  app.use('/singin',require('./singout'))
-  app.use('/singout',require('./singout'))
+  app.use('/signup',require('./signup'))
+  app.use('/signin',require('./signin'))
+  app.use('/signout',require('./signout'))
   app.use('/comments',require('./comments'))
   app.use('/posts',require('./posts'))
 }
